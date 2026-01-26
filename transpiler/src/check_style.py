79ee746391968e3feb639bc95d70a4520ce8bb38
Lily Dayton <lily.dayton.git@gmail.com>
2026-01-26 18:02:46 +0200

    remove unused check_style.py

--- transpiler/src/check_style.py
+++
@@ -1,8 +0,0 @@
-"""Enforce source subset restrictions."""
-
-from __future__ import annotations
-
-
-def check(source: str) -> list[str]:
-    """Return list of style violations."""
-    raise NotImplementedError
